d6191895e943f2dccc09c591105915db7f72318b
frohn <frohner.michael@gmail.com>
2020-12-14 16:17:15 +0100

    initial 2

--- basic.py
+++ basic.py
@@ -9,6 +9,7 @@ import math
 from pydispatch import dispatcher
 from datetime import datetime
 from queue import Queue
+
 run_once = True
 
 py_filename_without_extension = ""
@@ -18,6 +19,7 @@ xl_filename = ""
 stop_thread = False
 error_cell_address = "A1"
 status_cell_address = "B1"
+active_devices = []
 
 
 # --------------------------------------------------------------------------
@@ -56,7 +58,6 @@ def main():
 
 # --------------------------------------------------------------------------
 def start(interval, count):
-
     if xl_filename == (""):
         main()
 
@@ -65,7 +66,7 @@ def start(interval, count):
     wb.sheets.active.api.OLEObjects("MessageBox").Object.Value = "Started...\n"
 
     combo = 'ComboBox1'
-    wb.sheets.active.api.OLEObjects(combo).Object.AddItem('NTP-5431')   # active = aktuelle Tabelle
+    wb.sheets.active.api.OLEObjects(combo).Object.AddItem('NTP-5431')  # active = aktuelle Tabelle
     wb.sheets.active.api.OLEObjects(combo).Object.AddItem("SM2400")
     wb.sheets.active.api.OLEObjects(combo).Object.AddItem("BK-E2831")
     # wb.api.ActiveSheet.OLEObjects(combo).Object.ColumnCount = 2
@@ -74,7 +75,7 @@ def start(interval, count):
     # wb.sheets.active.api.OLEObjects("ComboBox").Object.Value = "some value"
     if isinstance(interval, str):
         try:
-            interval = interval.replace(",", ".") #Komma von , auf . ändern
+            interval = interval.replace(",", ".")  # Komma von , auf . ändern
             interval = float(interval)
         except:
             interval = 0
@@ -109,7 +110,7 @@ def measure(t, count):
     global client
 
     # accessing the sheet with index 0
-    sht = xw.Book(xl_filename).sheets[0]    #sheet 0 = 1.Tabelle
+    sht = xw.Book(xl_filename).sheets[0]  # sheet 0 = 1.Tabelle
 
     # Status zelle Text ändern und umfärben
     sht.range(status_cell_address).api.Font.Color = xwu.rgb_to_int((0, 0, 0))  # font color of text
@@ -135,9 +136,7 @@ def measure(t, count):
         # actuelle Zelle färben
         sht.range(cell).color = xwu.rgb_to_int((0, 200, 10))
 
-#-------------------------------------------
-
-
+        # -------------------------------------------
 
         global run_once
         if run_once:
@@ -156,7 +155,7 @@ def measure(t, count):
         cell_cur = (Zeile, 13)
         sht.range(cell_cur).value = str(Current)
 
-#-------------------------------------------
+        # -------------------------------------------
 
         # timing
         timer = 0
@@ -178,7 +177,7 @@ def measure(t, count):
 
 
 # --------------------------------------------------------------------------
-#main()
+# main()
 
 def mqtt_loop(_client):
     while True:
@@ -186,10 +185,10 @@ def mqtt_loop(_client):
 
 
 def on_connect(_client, userdata, flags, rc):
+    global active_devices
     print("Connected with result code " + str(rc))
     _client.subscribe("maqlab/#")
 
-    _client.publish("maqlab/user1/cmd/?")
     # _client.subscribe("maqlab/+/rep/#")
 
 
@@ -209,14 +208,15 @@ def receive_handler(message):
     # print('message in receive_handler: {}'.format(message))
     global run_once
     global starttime
+    global active_devices
     # print(message)
     if message.split("|")[0] == 'maqlab/ping/':
         message_splitted = message.split("|")
         t = float(message_splitted[1])
-        t = t+7200  #+2h aufgrund falscher Zeitzone
-        print(datetime.utcfromtimestamp(t).strftime('%Y-%m-%d %H:%M:%S'))  #unix timestamp in Datum und Zeit
+        t = t + 7200  # +2h aufgrund falscher Zeitzone
+        print(datetime.utcfromtimestamp(t).strftime('%Y-%m-%d %H:%M:%S'))  # unix timestamp in Datum und Zeit
 
-        if run_once:                # globale Variable, dass man nur einmal in die Funktion kommt
+        if run_once:  # globale Variable, dass man nur einmal in die Funktion kommt
             run_once = False
             starttime = t
 
@@ -227,22 +227,23 @@ def receive_handler(message):
 
     if '/accessnumber' in message:
         accessnumber = message.split("|")[1]
-        topic =  message.split("|")[0]
+        topic = message.split("|")[0]
         device_name = topic.split("/")[3]
         # message_new = message.replace('maqlab/user1/rep/', '')
         # message_new = message_new.replace('/accessnumber', '')
-        print(device_name, accessnumber)
-        #exec("%s = %d" % (message_new.split("|")[0], int(message_new.split("|")[1])))     # Topic als Variable ihren Wert zuweisen
-        #print(message_new)
+        # print(device_name, accessnumber)
+        active_devices.append((device_name, accessnumber))
+        # exec("%s = %d" % (message_new.split("|")[0], int(message_new.split("|")[1])))     # Topic als Variable ihren Wert zuweisen
+        # print(message_new)
 
-        #n = 1
-        #Geraet[n] = message_new.split("|")[0]
+        # n = 1
+        # Geraet[n] = message_new.split("|")[0]
 
-        #n = n+1
+        # n = n+1
 
+    # vars()[message_new.split("|")[0]] = message_new.split("|")[1]
+    # print(message_new.split("|")[0])
 
-    #vars()[message_new.split("|")[0]] = message_new.split("|")[1]
-    #print(message_new.split("|")[0])
 
 '''
     if message.split("|")[0] == 'maqlab/eingeraet/volt':
@@ -251,8 +252,6 @@ def receive_handler(message):
     if message.split("|")[0] == 'maqlab/eingeraet/current':
         print("Current: " + str(message.split("|")[1]) + "A")
 '''
-
-
 
 # Press the green button in the gutter to run the script.
 
@@ -268,7 +267,11 @@ if __name__ == '__main__':
     thread.start()
 
     dispatcher.connect(receive_handler, signal="receive", sender="mqtt")
-
+    time.sleep(1)
+    active_devices.clear()
+    client.publish("maqlab/user1/cmd/?")
+    time.sleep(1)
+    print(active_devices)
     while True:
         time.sleep(1)
         # client.publish("topic", "payload");
@@ -276,4 +279,3 @@ if __name__ == '__main__':
 
     # thread.join()
     # print("thread finished...exiting")
-
